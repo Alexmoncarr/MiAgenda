6778ef11d958406921650b89761c8cecf1441631
Alejandro <115974751+Alexmoncarr@users.noreply.github.com>
2023-11-03 11:34:21 +0100

    commit sqlite

--- app/src/main/java/com/example/miagenda/MainActivity.java
+++ app/src/main/java/com/example/miagenda/MainActivity.java
@@ -1,23 +1,20 @@
 package com.example.miagenda;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.SharedPreferences;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.text.InputType;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Scroller;
 import android.widget.Toast;
-import android.util.Log;
 
-import com.example.miagenda.R;
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editText1, editText2;
+    private DatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,35 +24,57 @@ public class MainActivity extends AppCompatActivity {
         editText1 = (EditText)findViewById(R.id.editText1);
         editText2 = (EditText)findViewById(R.id.editText2);
 
+        dbHelper = new DatabaseHelper(this);
     }
 
     //Método para guardar los datos:
     public void guardar(View view) {
-        String nombre= editText1.getText().toString();
-        String datos= editText2.getText().toString();
+        String nombre = editText1.getText().toString();
+        String datos = editText2.getText().toString();
 
+        // Obtiene la base de datos en modo escritura
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
 
-        SharedPreferences sharedPreferences = getSharedPreferences("Contactos", MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString(nombre, datos);
-        editor.commit();
-        finish();
+        // Crea un nuevo mapa de valores, donde las columnas son los nombres de las columnas
+        ContentValues values = new ContentValues();
+        values.put("nombre", nombre);
+        values.put("datos", datos);
 
-        Toast.makeText(this, "Contacto guardado", Toast.LENGTH_SHORT).show();
+        // Inserta la nueva fila, retorna el valor de clave primaria de la nueva fila
+        long newRowId = db.insert("Contactos", null, values);
+
+        Toast.makeText(this, "Contacto guardado con ID: " + newRowId, Toast.LENGTH_SHORT).show();
     }
 
     //Método para recuperar los datos:
     public void buscar(View view) {
-        String nombre= editText1.getText().toString();
-        SharedPreferences sharedPreferences = getSharedPreferences("Contactos", MODE_PRIVATE);
+        String nombre = editText1.getText().toString();
 
-        String datos = sharedPreferences.getString(nombre, "");
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
 
-        if (datos.isEmpty()){
-            Toast.makeText(this, "No se ha encontrado ningún dato", Toast.LENGTH_SHORT).show();
+        // Define una proyección que especifica qué columnas del resultado quieres obtener
+        String[] projection = {"datos"};
 
-        }else {
+        // Filtro para la cláusula WHERE
+        String selection = "nombre = ?";
+        String[] selectionArgs = {nombre};
+
+        Cursor cursor = db.query(
+                "Contactos",   // La tabla para consultar
+                projection,    // Las columnas para retornar
+                selection,     // Las columnas para la cláusula WHERE
+                selectionArgs, // Los valores para la cláusula WHERE
+                null,          // Agrupar las filas
+                null,          // Filtrar por filas que agrupan
+                null           // El orden de las filas
+        );
+
+        if (cursor.moveToFirst()) {
+            String datos = cursor.getString(cursor.getColumnIndexOrThrow("datos"));
             editText2.setText(datos);
+        } else {
+            Toast.makeText(this, "No se ha encontrado ningún dato", Toast.LENGTH_SHORT).show();
         }
+        cursor.close();
     }
 }
